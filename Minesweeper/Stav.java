bfdcdf13765445cadad7db248af6aeffb454a2e4
Nojpi <neupauer8@stud.uniza.sk>
2025-11-27 15:52:37 +0100

    Vytvorenie BlueJ
    A tried

---
+++ Minesweeper/Stav.java
@@ -0,0 +1,11 @@
+
+/**
+ * Vymenovacia trieda {@code Stav} slúži «doplňte opis»…
+ *
+ * @author   «meno autora»
+ * @version  «verzia alebo dátum»
+ */
+public enum Stav
+{
+    PONDELOK, UTOROK, STREDA, ŠTVRTOK, PIATOK, SOBOTA, NEDEĽA
+}
