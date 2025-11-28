8a3262decae9b145a595d20285dc5923688b2125
Nojpi <neupauer8@stud.uniza.sk>
2025-11-28 11:25:11 +0100

    Rozrobene pole a grid

--- Minesweeper/Grid.java
+++ Minesweeper/Grid.java
@@ -10,28 +10,35 @@ public class Grid
     // Nahraďte nižšie uvedený príklad vlastnými premennými…
 
     // inštančné premenné…
-    private int x;
+    private Pole[][] policka;    
+    private int sirka;
+    private int vyska;
+    private int velkostPolicka;
 
 
     /**
      * Konštruktory objektov triedy Grid.
      */
-    public Grid()
-    {
-        // Inicializujte inštančné premenné.
-        x = 0;
+    public Grid(int sirka, int vyska, int velkostPolicka) {
+        this.sirka = sirka;
+        this.vyska = vyska;
+        this.velkostPolicka = velkostPolicka;
+        this.policka = new Pole[vyska][sirka];      
+        for (int y = 0; y < vyska; y++) {
+            for (int x = 0; x < sirka; x++) {
+                this.policka[y][x] = new Pole(x, y, velkostPolicka);
+            }
+        }
+    }
+    
+    public void zaber(int x, int y) {
+        Pole p = this.policka[y][x];
+        if (p.getNieJeOdhalene() == true ) {
+            p.odhal();
+        } else if (p.getJeOdhalene() == true) {
+            p.jeOdhalene();
+        }
+        }
     }
 
-
-    /**
-     * Prázdna metóda – «tento komentár nahraďte vlastným opisom»…
-     *
-     * @param   y  opis významu parametra
-     * @return     opis významu návratovej hodnoty – napr.: súčet x a y
-     */
-    public int príkladMetódy(int y)
-    {
-        // Sem vložte príkazy tela metódy.
-        return x + y;
-    }
-}
+    
--- Minesweeper/Pole.java
+++ Minesweeper/Pole.java
@@ -4,31 +4,50 @@
  * @author   «meno autora»
  * @version  «verzia alebo dátum»
  */
+import fri.shapesge.Stvorec; 
+
 public class Pole {
-    private Boolean JeMina;
-    private boolean JeOdhalene;
+    private Stvorec stvorec;
+    private Boolean jeMina;
+    private boolean jeOdhalene;
+    private boolean nieJeOdhalene;
     
-    private int x;
 
     /**
      * Konštruktory objektov triedy Pole.
      */
-    public Pole()
-    {
-        // Inicializujte inštančné premenné.
-        x = 0;
+    public Pole(int x, int y, int velkost) {
+        this.stvorec = new Stvorec();
+        this.stvorec.zmenStranu(velkost - 1);
+        this.stvorec.posunVodorovne(-60 + x * velkost);
+        this.stvorec.posunZvisle(-50 + y * velkost);
+        this.stvorec.zobraz();
+        zaberPole();
+    }
+    public void odhal(){
+        this.nieJeOdhalene = true;
+        this.jeOdhalene = false;
+        this.zaberPole();
+    }
+    public void jeOdhalene(){
+        this.nieJeOdhalene = false;
+        this.jeOdhalene = true;
+        this.zaberPole();
+    }
+    public void zaberPole() {
+        if (this.jeOdhalene = false) {
+            this.stvorec.zmenFarbu("#B1C9EE");
+        } else if (this.jeOdhalene = true) {
+            this.stvorec.zmenFarbu("#CFDEE7");
+        } 
+    }
+    public boolean getJeOdhalene(){
+        return this.jeOdhalene;
+    }
+    public boolean getNieJeOdhalene(){
+        return this.nieJeOdhalene;
     }
 
 
-    /**
-     * Prázdna metóda – «tento komentár nahraďte vlastným opisom»…
-     *
-     * @param   y  opis významu parametra
-     * @return     opis významu návratovej hodnoty – napr.: súčet x a y
-     */
-    public int príkladMetódy(int y)
-    {
-        // Sem vložte príkazy tela metódy.
-        return x + y;
-    }
+    
 }
